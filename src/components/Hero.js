ff8aacb53d135bfb7163136526726cf48e7854ec
mnbk21 <mnbk21@gmail.com>
2024-12-19 23:01:01 +0900

    Statsコンポーネント完成

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
